e5c93281b0986aad005cb90ee0d513facf2a3bc3
Arne Kobald <arne@krypton.ipc.uni-tuebingen.de>
2018-04-25 17:58:42 +0000

    Update SHTC1.py

--- micropython/test_board_3/SHTC1.py
+++ micropython/test_board_3/SHTC1.py
@@ -1,3 +1,25 @@
+# The MIT License (MIT)
+#
+# Copyright (c) 2018 AG Weimar 
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
 import time
 from machine import I2C
 import machine
@@ -46,11 +68,10 @@ class SHTC1_Sensor:
         #print("\tOK Data: ", [hex(i) for i in result])
         return result
 
-    # pylint: disable=no-self-use
     def _generate_crc(self, data):
         """8-bit CRC algorithm for checking data"""
         crc = 0xFF
-        # calculates 8-Bit checksum with given polynomial
+        # Calculate 8-Bit checksum with given polynomial
         for byte in data:
             crc ^= byte
             for _ in range(8):
